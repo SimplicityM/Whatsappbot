c0768f9c9f1cc35d93ac2804d31feafd8a5f63a0
Abdulkareem Onimisi Moshood <moshood2uall@gmail.com>
2025-10-31 02:57:59 +0100

    make changes

--- Public/util/emailMarketing.js
+++ Public/util/emailMarketing.js
@@ -1,9 +1,19 @@
 const nodemailer = require('nodemailer'); const cron = require('node-cron');
 
-class EmailMarketing { constructor() { this.transporter = nodemailer.createTransporter({ host: process.env.SMTP_HOST, port: process.env.SMTP_PORT, secure: true, auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS } });
-
-    this.setupCronJobs();
-}
+class EmailMarketing {
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: true,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
+            }
+        });
+        
+        this.setupCronJobs();
+    }
 
 setupCronJobs() {
     // Daily email campaigns
