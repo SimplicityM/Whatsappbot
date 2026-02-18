3fd15a407bba782cb9df6d8fe3cad81f5bfe4303
Abdulkareem Onimisi Moshood <moshood2uall@gmail.com>
2026-02-18 09:40:26 +0100

    make changes

--- server/server.js
+++ server/server.js
@@ -1501,7 +1501,7 @@ app.get('/api/sessions/status/:sessionId', authenticate, async (req, res) => {
     }
     
 });
-        const PORT = process.env.PORT || 3000;
+        const PORT = process.env.PORT || 5000;
     server.listen(PORT, () => {
         serverStarted = true; // Mark as started
         console.log(`🚀 Server running on port ${PORT}`);
